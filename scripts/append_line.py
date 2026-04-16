fe8fde3fd6b6f3353f718aa43340f9ad27fe6900
Rafael Bernardo <rafaelbnneto@gmail.com>
2026-04-16 15:58:01 -0300

    push example worflow to test changing files through workflow

---
+++ scripts/append_line.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+if __name__ == "__main__":
+    current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    new_line = f"NE;PE;POCAO;RAFAEL NETO; 00.000.000/0001-00;ESTRADA VICINAL MARIO COVAS;4189;;BAIRRO DE CIMA;18404-517;GLP;{current_datetime};115,99;;R$ / 13 kg;SUPERGASBRAS ENERGIA"
+    with open("dados-abertos-precos-glp.csv", "a", encoding="UTF-8") as f:
+        f.write("\n" + new_line)
+    print("Teoricamente adicionei a linha")
